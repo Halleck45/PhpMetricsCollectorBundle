69cb8da86dadd6ee7f513f40758ac9a921835431
Oskar Pfeifer-Bley <pfeifer-bley@ipa-dguv.de>
2017-12-12 08:32:21 +0100

    Adjust for new version of PhpMetrics

--- src/Hal/Bundle/PhpMetricsCollector/Collector/PhpMetricsCollector.php
+++ src/Hal/Bundle/PhpMetricsCollector/Collector/PhpMetricsCollector.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace Hal\Bundle\PhpMetricsCollector\Collector;
 
-use Hal\Component\Token\Tokenizer;
-use Hal\Component\Token\TokenType;
-use Hal\Metrics\Complexity\Component\McCabe\McCabe;
-use Hal\Metrics\Complexity\Text\Halstead\Halstead;
-use Hal\Metrics\Complexity\Text\Length\Loc;
-use Hal\Metrics\Design\Component\MaintainabilityIndex\MaintainabilityIndex;
+use Hal\Application\Analyze;
+use Hal\Application\Config\Config;
+use Hal\Component\Issue\Issuer;
+use Hal\Component\Output\TestOutput;
+use Hal\Metric\ClassMetric;
+use Hal\Metric\Consolidated;
+use Hal\Metric\InterfaceMetric;
+use Hal\Metric\Metric;
+use Hal\Violation\Violations;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
@@ -15,87 +19,82 @@ class PhpMetricsCollector extends DataCollector
 {
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
-
         // filter loaded files
         $files = get_included_files();
-        $files = array_filter($files, function ($v) {
-            $excludes = array('vendor', 'pear', '\\.phar', 'bootstrap\.php', 'Test', '/app', 'AppKernel.php', 'autoload.php', 'cache/', 'app.php', 'app_dev.php', 'Form', 'PhpMetrics', 'classes.php');
-            return !preg_match('!' . implode('|', $excludes) . '!', $v);
+        $excludes = [
+            'vendor',
+            'pear',
+            '\\.phar',
+            'bootstrap\.php',
+            'Test',
+            '/app',
+            'AppKernel.php',
+            'autoload.php',
+            'cache/',
+            'app.php',
+            'app_dev.php',
+            'Form',
+            'classes.php',
+        ];
+        $files = array_filter($files, function ($filePath) use ($excludes) {
+            return ! preg_match(
+                '!' . implode('|', $excludes) . '!',
+                mb_substr($filePath, mb_strlen(getcwd()))
+            );
         });
-
-        // Prepare datas
-        $all = $average = array(
-            'cfiles' => 0,
-            'maintainability' => array(),
-            'commentWeight' => array(),
-            'complexity' => array(),
-            'loc' => array(),
-            'lloc' => array(),
-            'cloc' => array(),
-            'bugs' => array(),
-            'difficulty' => array(),
-            'intelligentContent' => array(),
-            'vocabulary' => array(),
-        );
-        $scoreByFile = array();
-
-        // group files into tmp folder
-        $folder = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid();
-        mkdir($folder);
-        foreach ($files as $file) {
-            // run PhpMetrics
-            $tokenizer = new Tokenizer();
-            $tokenType = new TokenType();
-
-            // halstead
-            $halstead = new Halstead($tokenizer, $tokenType);
-            $rHalstead = $halstead->calculate($file);
-
-            // loc
-            $loc = new Loc($tokenizer);
-            $rLoc = $loc->calculate($file);
-
-            // complexity
-            $complexity = new McCabe($tokenizer);
-            $rComplexity = $complexity->calculate($file);
-
-            // maintainability
-            $maintainability = new MaintainabilityIndex();
-            $rMaintenability = $maintainability->calculate($rHalstead, $rLoc, $rComplexity);
-
-            // store result
-            $files[$file] = array();
-            $all['cfiles']++;
-            $all['maintainability'][] = $scoreByFile[$file]['maintainability'] = $rMaintenability->getMaintainabilityIndex();
-            $all['commentWeight'][] = $scoreByFile[$file]['commentWeight'] = $rMaintenability->getCommentWeight();
-            $all['complexity'][] = $scoreByFile[$file]['complexity'] = $rComplexity->getCyclomaticComplexityNumber();
-            $all['loc'][] = $scoreByFile[$file]['loc'] = $rLoc->getLoc();
-            $all['lloc'][] = $scoreByFile[$file]['lloc'] = $rLoc->getLogicalLoc();
-            $all['cloc'][] = $scoreByFile[$file]['cloc'] = $rLoc->getCommentLoc();
-            $all['bugs'][] = $scoreByFile[$file]['bugs'] = $rHalstead->getBugs();
-            $all['difficulty'][] = $scoreByFile[$file]['difficulty'] = $rHalstead->getDifficulty();
-            $all['intelligentContent'][] = $scoreByFile[$file]['intelligentContent'] = $rHalstead->getIntelligentContent();
-            $all['vocabulary'][] = $scoreByFile[$file]['vocabulary'] = $rHalstead->getVocabulary();
+        $config = new Config();
+        $config->set('exclude', $excludes);
+        $config->set('files', $files);
+        $metrics = (new Analyze($config, new TestOutput(), new Issuer(new TestOutput())))->run($files);
+        foreach ($metrics->all() as $each) {
+            $each->set('violations', new Violations());
+        }
+        $classMetrics = array_filter($metrics->all(), function (Metric $metric) {
+            return $metric instanceof ClassMetric
+                || $metric instanceof InterfaceMetric;
+        });
+        $consolidated = new Consolidated($metrics);
+        $scoreByClass = [];
+        /** @var ClassMetric|InterfaceMetric $each */
+        foreach ($classMetrics as $each) {
+            $scoreByClass[$each->getName()] = [
+                'maintainability' => $each->get('mi'),
+                'commentWeight' => $each->get('commentWeight'),
+                'complexity' => $each->get('ccn'),
+                'loc' => $each->get('loc'),
+                'lloc' => $each->get('lloc'),
+                'cloc' => $each->get('cloc'),
+                'bugs' => $each->get('bugs'),
+                'difficulty' => $each->get('difficulty'),
+                'intelligentContent' => $each->get('intelligentContent'),
+                'vocabulary' => $each->get('vocabulary'),
+            ];
         }
 
         // average
-        if ($all['cfiles'] > 0) {
-            $average['maintainability'] = array_sum($all['maintainability']) / sizeof($all['maintainability']);
-            $average['commentWeight'] = array_sum($all['commentWeight']) / sizeof($all['commentWeight']);
-            $average['complexity'] = array_sum($all['complexity']) / sizeof($all['complexity']);
-            $average['loc'] = array_sum($all['loc']);
-            $average['lloc'] = array_sum($all['lloc']);
-            $average['cloc'] = array_sum($all['cloc']);
-            $average['bugs'] = array_sum($all['bugs']) / sizeof($all['bugs']);
-            $average['difficulty'] = array_sum($all['difficulty']) / sizeof($all['difficulty']);
-            $average['intelligentContent'] = array_sum($all['intelligentContent']) / sizeof($all['intelligentContent']);
-            $average['vocabulary'] = array_sum($all['vocabulary']) / sizeof($all['vocabulary']);
+        $average = [];
+        if (count($classMetrics) > 0) {
+            $calculateAvg = function ($property) use ($classMetrics) {
+                return array_sum(array_map(function (Metric $classMetric) use ($property) {
+                        return $classMetric->get($property);
+                    }, $classMetrics)) / count($classMetrics);
+            };
+            $average['maintainability'] = $consolidated->getAvg()->mi;
+            $average['commentWeight'] = $consolidated->getAvg()->commentWeight;
+            $average['complexity'] = $consolidated->getAvg()->ccn;
+            $average['loc'] = $calculateAvg('loc');
+            $average['lloc'] = $calculateAvg('lloc');
+            $average['cloc'] = $calculateAvg('cloc');
+            $average['bugs'] = $consolidated->getAvg()->bugs;
+            $average['difficulty'] = $consolidated->getAvg()->difficulty;
+            $average['intelligentContent'] = $consolidated->getAvg()->intelligentContent;
+            $average['vocabulary'] = $calculateAvg('vocabulary');
         }
-        $this->data = array(
+        $this->data = [
             'average' => $average,
-            'cfiles' => $all['cfiles'],
-            'files' => $scoreByFile
-        );
+            'cclasses' => count($classMetrics),
+            'classes' => $scoreByClass,
+        ];
     }
 
     public function getMaintainabilityIndex()
@@ -148,13 +147,14 @@ class PhpMetricsCollector extends DataCollector
         return $this->data['average']['vocabulary'];
     }
 
-    public function getNumberOfFiles()
+    public function getNumberOfClasses()
     {
-        return $this->data['cfiles'];
+        return $this->data['cclasses'];
     }
 
-    public function getFiles() {
-        return $this->data['files'];
+    public function getClasses()
+    {
+        return $this->data['classes'];
     }
 
     public function getName()
--- testing/application/src/My/Controller/MyController.php
+++ testing/application/src/My/Controller/MyController.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace My\Controller;
+
 use Symfony\Component\HttpFoundation\Response;
 
-class MyController {
-    public function route1() {
-       return new Response('<html><body>Hello world</body></html>', 200);
+class MyController
+{
+    public function route1()
+    {
+        return new Response('<html><body>Hello world</body></html>', 200);
     }
 }
